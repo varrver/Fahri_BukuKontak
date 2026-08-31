52d368ca843f288b9ee07c89d8b60cda3a93d2b7
varrver <aonosensou@gmail.com>
2026-08-31 11:13:31 +0700

    feat: add navigation, tab view, and about page to contact application

--- lib/main.dart
+++ lib/main.dart
@@ -5,124 +5,248 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Buku Kontak', home: KontakPage());
+    return MaterialApp(
+      title: 'Buku Kontak',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(color: Colors.blue),
+      ),
+      home: const DefaultTabController(length: 2, child: HomePage()),
+    );
   }
 }
 
-class Kontak {
-  final String nama;
+class Contact {
+  final String name;
   final String email;
-  final String noHp;
+  final String phone;
 
-  Kontak({required this.nama, required this.email, required this.noHp});
+  Contact({required this.name, required this.email, required this.phone});
 }
 
-class KontakPage extends StatefulWidget {
-  const KontakPage({super.key});
+final List<Contact> _contacts = [];
+
+// Halaman Beranda
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<KontakPage> createState() => _KontakPageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _KontakPageState extends State<KontakPage> {
-  final List<Kontak> _listKontak = [];
-  
-  final TextEditingController _namaController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _noHpController = TextEditingController();
-
-  void _simpanKontak() {
-    if (_namaController.text.isNotEmpty &&
-        _emailController.text.isNotEmpty &&
-        _noHpController.text.isNotEmpty) {
-      setState(() {
-        _listKontak.add(
-          Kontak(
-            nama: _namaController.text,
-            email: _emailController.text,
-            noHp: _noHpController.text,
-          ),
-        );
-        _namaController.clear();
-        _emailController.clear();
-        _noHpController.clear();
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    _namaController.dispose();
-    _emailController.dispose();
-    _noHpController.dispose();
-    super.dispose();
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Buku Kontak', style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.blue,
+        title: const Text('Buku Kontak'),
+        bottom: const TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.person), text: 'Kontak'),
+            Tab(icon: Icon(Icons.star), text: 'Favorit'),
+          ],
+        ),
       ),
+      body: const TabBarView(
+        children: [
+          ContactPage(),
+          Center(child: Text('Belum ada kontak favorit.')),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text('Buku Kontak'),
+            ),
+            ListTile(
+              title: const Text('Kontak'),
+              leading: Icon(Icons.contact_page),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Tambah Kontak'),
+              leading: Icon(Icons.add),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddContactPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Favorit'),
+              leading: Icon(Icons.star),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Tentang'),
+              leading: Icon(Icons.info),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AboutPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Halaman Kontak
+class ContactPage extends StatefulWidget {
+  const ContactPage({super.key});
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                TextField(
-                  controller: _namaController,
-                  decoration: const InputDecoration(labelText: 'Nama Lengkap'),
-                ),
-                const SizedBox(height: 8.0),
-                TextField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(labelText: 'Email'),
-                ),
-                const SizedBox(height: 8.0),
-                TextField(
-                  controller: _noHpController,
-                  keyboardType: TextInputType.phone,
-                  decoration: const InputDecoration(labelText: 'No Handphone'),
-                ),
-                const SizedBox(height: 16.0),
-                ElevatedButton(
-                  onPressed: _simpanKontak,
-                  child: const Text('Simpan'),
-                ),
-              ],
-            ),
-          ),
-          
           Expanded(
             child: ListView.builder(
-              itemCount: _listKontak.length,
+              itemCount: _contacts.length,
               itemBuilder: (context, index) {
-                final kontak = _listKontak[index];
+                final contact = _contacts[index];
                 return ListTile(
-                  leading: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.person, color: Colors.black, size: 24),
-                    ],
-                  ),
-                  title: Text(
-                    kontak.nama,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text('${kontak.email}\n${kontak.noHp}'),
-                  isThreeLine: true,
+                  leading: Icon(Icons.person),
+                  title: Text(contact.name),
+                  subtitle: Text('${contact.email}\n${contact.phone}'),
                 );
               },
             ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddContactPage()),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// Halaman Tambah Kontak
+class AddContactPage extends StatefulWidget {
+  const AddContactPage({super.key});
+
+  @override
+  State<AddContactPage> createState() => _AddContactPageState();
+}
+
+class _AddContactPageState extends State<AddContactPage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+
+  void _addContact() {
+    if (_nameController.text.isNotEmpty &&
+        _emailController.text.isNotEmpty &&
+        _phoneController.text.isNotEmpty) {
+      setState(() {
+        _contacts.add(
+          Contact(
+            name: _nameController.text,
+            email: _emailController.text,
+            phone: _phoneController.text,
+          ),
+        );
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tambah Kontak')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: 'Nama Lengkap'),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _phoneController,
+                  decoration: const InputDecoration(labelText: 'No Handphone'),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    _addContact();
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Simpan'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Halaman Tentang
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tentang')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('assets/profile.jpg'),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Fahri Ferdyansyah',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              const Text('XII RPL B'),
+              const SizedBox(height: 20),
+              const Text('SMK Negeri 5 Surakarta'),
+            ],
+          ),
+        ),
       ),
     );
   }
